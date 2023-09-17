1fd4a6cefed38c76fff50fec888c8d6537ec86dc
Maxim Karassev <maximus_1901@mail.ru>
2023-09-18 01:01:25 +0600

    Clock_animated hometask 2

--- app/src/main/java/com/example/andersenhometask2views/MainActivity.kt
+++ app/src/main/java/com/example/andersenhometask2views/MainActivity.kt
@@ -3,50 +3,33 @@ package com.example.andersenhometask2views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.URLUtil
-import android.widget.Toast
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.example.andersenhometask2views.databinding.ActivityMainBinding
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val toastMessage = "Error, please enter URL correctly"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.buttonLoadImage.setOnClickListener {
-
-            val imageUrl = binding.editTextUrl.text.toString()
-
-            if (isValidUrl(imageUrl)) {
-                loadImage(imageUrl)
-            } else {
-                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
-            }
-
-        }
-
-
-    }
-
-    private fun isValidUrl(url: String): Boolean {
-        return URLUtil.isValidUrl(url)
-    }
-
-    private fun loadImage(imageURL: String) {
-
-        Glide.with(this)
-            .load(imageURL)
-            .skipMemoryCache(true)
-            .diskCacheStrategy(DiskCacheStrategy.NONE)
-            .into(binding.imageView)
-
+        setContentView(R.layout.activity_main)
+        val secondsClockLineImageView = findViewById<ImageView>(R.id.image_seconds_line)
+        val rotationAnim = RotateAnimation(
+            0f,
+            360f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.9f
+        )
+        rotationAnim.interpolator = LinearInterpolator()
+        rotationAnim.startOffset = 0
+        rotationAnim.duration = 60000
+        rotationAnim.repeatCount = Animation.INFINITE
+        secondsClockLineImageView.startAnimation(rotationAnim)
 
     }
 
