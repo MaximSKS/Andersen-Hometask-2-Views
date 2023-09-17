58308f54337409dfc0103623a7dd365e9f7c5a09
Maxim Karassev <maximus_1901@mail.ru>
2023-09-17 19:16:02 +0600

    Link to picture hometask 2

--- app/src/main/java/com/example/andersenhometask2views/MainActivity.kt
+++ app/src/main/java/com/example/andersenhometask2views/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.andersenhometask2views
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.URLUtil
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.andersenhometask2views.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val toastMessage = "Error, please enter URL correctly"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLoadImage.setOnClickListener {
+
+            val imageUrl = binding.editTextUrl.text.toString()
+
+            if (isValidUrl(imageUrl)) {
+                loadImage(imageUrl)
+            } else {
+                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
+
+    private fun isValidUrl(url: String): Boolean {
+        return URLUtil.isValidUrl(url)
+    }
+
+    private fun loadImage(imageURL: String) {
+
+        Glide.with(this)
+            .load(imageURL)
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(binding.imageView)
+
+
+    }
+
 }
+
